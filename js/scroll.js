f3699a7a8338ddf643cdf1e2c7d34d599a63518e
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2023-11-01 16:05:27 -0400

    Add scroll to top button: HTML, CSS, JS.

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,21 @@
-// TODO: scroll button function
-
 // Export
 export function initScrollToTopButton() {
-  
+  const scrollBtn = document.querySelector('.scroll-btn');
+
+  // when user scrolls below navbar, show scroll button
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+      scrollBtn.classList.add('active');
+    } else {
+      scrollBtn.classList.remove('active');
+    }
+  });
+
+  // when user clicks scroll button, scroll to top
+  scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
 }
