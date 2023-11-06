522c922de265900c586e6fb6b42ede3c99cd56ef
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2023-11-06 12:48:16 -0500

    Code js module for index.html slider

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,18 @@
 export function initSlideOnScroll() {
-  
+  // scroll effect
+  function moveSlider() {
+    const slider = document.querySelector('.slider');
+    const sliderRect = slider.getBoundingClientRect();
+
+    // if slider is in view
+    if (sliderRect.top < window.innerHeight && sliderRect.bottom > 0) {
+      const scrollPercent = (sliderRect.bottom / window.innerHeight) * 100;
+      const offset = 50;
+      const translateX = scrollPercent - (110 - offset);
+
+      slider.style.transform = `translateX(${translateX}%)`;
+    }
+  }
+
+  window.addEventListener('scroll', moveSlider);
 }
